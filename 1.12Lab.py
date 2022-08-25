7145fc33280c09fa81737f87273fbe0cd597c86f
Sai Charan Todupunoori <tsaicharan03@gmail.com>
2022-08-24 23:05:32 -0500

    Finished labs 1.12 and 1.20

---
+++ 1.12Lab.py
@@ -0,0 +1,7 @@
+#Sai Charan Todupunoori
+#PSID: 2048092
+if __name__ == '__main__':
+    userNum = int(input())
+    userNumSquared = userNum * userNum
+    print(userNumSquared)
+
---
+++ 1.20Lab.py
@@ -0,0 +1,15 @@
+#Sai Charan Todupunoori
+#PSID: 2048092
+
+integer = int(input("Enter integer:\n"))
+print("You entered:", integer)
+integerS = integer * integer
+print(integer, "squared is", integerS)
+integerC = integer * integer * integer
+print("And", integer, "cubed is", integerC, "!!")
+
+integer2 = int(input("Enter another integer:\n"))
+total = integer + integer2
+prod = integer * integer2
+print(integer, "+", integer2, "is", total)
+print(integer, "*", integer2, "is", prod)
