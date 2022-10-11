af50de54eb0669bfcccd7e8d62c84e9176321753
Sai Charan Todupunoori <tsaicharan03@gmail.com>
2022-10-11 12:43:05 -0500

    Finished 9.10 ZyLab

---
+++ Homework02/9.10Lab.py
@@ -0,0 +1,14 @@
+import csv
+
+file_name = input()
+
+word_list = []
+
+with open(str(file_name), 'r') as csvfile:
+    user_file = csv.reader(csvfile, delimiter=',')
+
+    for row in user_file:
+        for index in range(len(row)):
+            if row[index] not in word_list:
+                print(f'{row[index]} {row.count(row[index])}')
+                word_list.append(row[index])
